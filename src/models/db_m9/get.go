402caf1b733f7441ccb2a7947de23ea15b977afc
lzt <m404_error_collapsed@outlook.com>
2021-03-25 18:28:04 +0800

    3.25 新增其它表的Exist和Get函数

--- src/models/db_m9/get.go
+++ src/models/db_m9/get.go
@@ -80,6 +80,51 @@ func ExistACBranchByID(id int) (bool, error) {
 	return false, nil
 }
 
+func ExistTransformerByID(id int) (bool, error) {
+	var acb ACBranch
+	err := models.M9.Model(&Transformer{}).Where("id = ?", id).First(&acb).Error
+	//err:=models.M9.Raw("SELECT id, type FROM "+table+" WHERE id = ?", id).Scan(&res).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return false, err
+	}
+
+	if acb.ID > 0 {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+func ExistDCBranchByID(id int) (bool, error) {
+	var acb ACBranch
+	err := models.M9.Model(&DCBranch{}).Where("id = ?", id).First(&acb).Error
+	//err:=models.M9.Raw("SELECT id, type FROM "+table+" WHERE id = ?", id).Scan(&res).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return false, err
+	}
+
+	if acb.ID > 0 {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+func ExistConvertorByID(id int) (bool, error) {
+	var acb ACBranch
+	err := models.M9.Model(&Convertor{}).Where("id = ?", id).First(&acb).Error
+	//err:=models.M9.Raw("SELECT id, type FROM "+table+" WHERE id = ?", id).Scan(&res).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return false, err
+	}
+
+	if acb.ID > 0 {
+		return true, nil
+	}
+
+	return false, nil
+}
+
 func GetACBranchByID(id int) (*ACBranch, error) {
 	var acb ACBranch
 	err := models.M9.Model(&ACBranch{}).Where("id = ? and `delete` != 1", id).First(&acb).Error
@@ -87,4 +132,31 @@ func GetACBranchByID(id int) (*ACBranch, error) {
 		return nil, err
 	}
 	return &acb, nil
+}
+
+func GetTransformerByID(id int) (*Transformer, error) {
+	var trans Transformer
+	err := models.M9.Model(&Transformer{}).Where("id = ? and `delete` != 1", id).First(&trans).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return nil, err
+	}
+	return &trans, nil
+}
+
+func GetDCBranchByID(id int) (*DCBranch, error) {
+	var dcb DCBranch
+	err := models.M9.Model(&DCBranch{}).Where("id = ? and `delete` != 1", id).First(&dcb).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return nil, err
+	}
+	return &dcb, nil
+}
+
+func GetConvertorByID(id int) (*Convertor, error) {
+	var cvt Convertor
+	err := models.M9.Model(&Convertor{}).Where("id = ? and `delete` != 1", id).First(&cvt).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return nil, err
+	}
+	return &cvt, nil
 }
